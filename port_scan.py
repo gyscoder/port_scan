8d2e539027d01ad13ed8798b4d4b713952d008f8
GYSCODER <geysomsilva.20@gmail.com>
2021-09-05 15:58:19 -0300

    port scan script

---
+++ port_scan.py
@@ -0,0 +1,43 @@
+#!/bin/python
+
+import sys
+import socket
+from datetime import datetime
+
+#set
+target = ''
+horario = str(datetime.now())
+#defina o alvo
+
+if len(sys.argv) == 2:
+        target = socket.gethostbyname(sys.argv[1]) #traduz o hostname para ipv4
+else:
+        print("[-] Quantia de argumentos invalidos! [-]")
+        print("[!] Sintaxe: python3 port_scan.py <ip/host> [!]")
+
+#estética, add um banner
+print("-" * 50)
+print("[!] Alvo de varredura {} [!]".format(target))
+print("[--:--] Tempo em que começou {} [--:--]".format(horario[0:19]))
+print("-" * 50)
+
+try:
+        for port in range(1, 65535):
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                socket.setdefaulttimeout(1)
+                result = s.connect_ex((target, port)) #retorna um erro, se a porta est>                #print("[+] Checando porta {}  [+]".format(port)) #< mostra todas as p>                if result == 0:
+                        print("[+] Porta {} esta aberta [+]".format(port))
+                s.close()
+
+except KeyboardInterrupt:
+        print("\n[!] Parando o scan [!]")
+        sys.exit()
+
+except socket.gaierror:
+        print("[!] Hostname nao pode ser resolvida  [!]")
+        sys.exit()
+
+except socket.error:
+        print("[!] Falha ao tentar se conectar ao servidor [!]")
+        sys.exit()
+
